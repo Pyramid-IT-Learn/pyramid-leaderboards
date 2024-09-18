d3b9471743f536709ec3c6dab65d76bb3a74d916
Rushyendra Guntupalli <74148058+dog-broad@users.noreply.github.com>
2024-09-18 18:22:43 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://cmrit-leaderboards-backend.vercel.app'; // Update this to your backend URL
+const API_URL = 'https://pyramid-leaderboards-backend.vercel.app'; // Update this to your backend URL
 
 export const fetchDatabases = () => {
   return axios.get(`${API_URL}/databases`);
